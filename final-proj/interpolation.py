3c20a802486ff0285027c3cd122993d8d1c22bbe
mrollinswaterman <miles.r.waterman@gmail.com>
2026-02-19 17:35:46 -0600

    [FINISHED] final project pre-submission complete. all tests passing on gradescope.

--- final-proj/interpolation.py
+++ final-proj/interpolation.py
@@ -1,7 +1,11 @@
 from typing import Sequence
 import numpy as np
-import matplotlib.pyplot as plt
 
+
+"""
+BSpline methodology from here:
+    - https://github.com/Liam-Xander/Simple-BSpline-Python/blob/main/Bsplne.py
+"""
 
 class BSpline:
     def __init__(
@@ -30,39 +34,48 @@ class BSpline:
         Evaluate the B-spline basis function i, k at input position x.
         (Note that i, k start at 0.)
         """
-        if k == 0:  # base case
-            # print("base case!")
-            if self.t[i] <= x and self.t[i + 1] > x:
-                return 1
+        if k == 0:
+            if (x >= self.t[i]) and (x < self.t[i + 1]):
+                return 1.0
             else:
-                return 0
+                return 0.0
         else:
-            # print("recurring...")
-            return (
-                (x - self.t[i]) / (self.t[i + k] - self.t[i]) * self.bases(x, k - 1, i)
-            ) + (
-                (self.t[i + k + 1] - x)
-                / (self.t[i + k + 1] - self.t[i + 1])
-                * self.bases(x, k - 1, i + 1)
-            )
+
+            length1 = self.t[i + k] - self.t[i]
+            length2 = self.t[i + k + 1] - self.t[i + 1]
+
+            if length1 == 0.0:
+                length1 = 1.0
+            if length2 == 0.0:
+                length2 = 1.0
+
+        term1 = (x - self.t[i]) / length1 * self.bases(x=x, k=k - 1, i=i)
+        term2 = (self.t[i + k + 1] - x) / length2 * self.bases(x=x, k=k - 1, i=i + 1)
+
+        return term1 + term2
 
     def interp(self, x: float) -> float:
         """Evaluate the B-spline at input position x."""
-        return self.bases(x=x, k=self.d, i=0)
+        #print("running interp...")
+        sum = 0.0
+        for i in range(len(self.c)):
+            sum += self.c[i] * self.bases(x=x, k=self.d, i=i)
+        return sum
+
 
 
 if __name__ == "__main__":
-    t = [0, 1, 2, 3, 4]  # set some knots. change this.
-    c = [0, 3]  # set some control points (such as control colors). change this.
+    t = [0, 1, 2, 3, 4, 5]  # set some knots. change this.
+    c = [0.6, 1, 0.2]  # set some control points (such as control colors). change this.
     d = 2  # set the degree.  change this.
     spline = BSpline(t, c, d)
     # now interpolate at some value
-    x = np.linspace(0, 3)
-    y = [spline.interp(v) for v in x]
+    x = 0.6122448979591836
+    print(spline.interp(x))
 
-    plt.plot(x, y)
-    plt.title("B-Spline Curve")
-    plt.xlabel("x")
-    plt.ylabel("S(x)")
-    plt.grid(True)
-    plt.show()
+    # plt.plot(x, y)
+    # plt.title("B-Spline Curve")
+    # plt.xlabel("x")
+    # plt.ylabel("S(x)")
+    # plt.grid(True)
+    # plt.show()
